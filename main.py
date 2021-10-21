6a1742365df3c630aa7b2bc0a31972f60180fb02
Marcus Lim <mj_lim0802@yahoo.com>
2021-10-21 02:52:14 -0500

    potential fix for conch shell

--- main.py
+++ main.py
@@ -783,37 +783,23 @@ async def ask(ctx):
                                 if msg.content.lower().startswith(word):
                                     if msg.content == "is daniel gay":
                                         choice = random.randint(1,2)
-                                        vc.play(discord.FFmpegPCMAudio(executable="C:/Users/mj_li/Desktop/discord bot/MP3_Files/ffmpeg-n4.4-175-ga2bb836e55-win64-gpl-4.4/bin/ffmpeg.exe", source="C:/Users/mj_li/Desktop/discord bot/MP3_Files/" + yes[choice]))
+                                        vc.play(discord.FFmpegPCMAudio(executable="MP3_Files/ffmpeg-n4.4-175-ga2bb836e55-win64-gpl-4.4/bin/ffmpeg.exe", source="MP3_Files/" + yes[choice]))
                                         break
                                     elif msg.content.lower().startswith("guh"):
-                                        vc.play(discord.FFmpegPCMAudio(executable="C:/Users/mj_li/Desktop/discord bot/MP3_Files/ffmpeg-n4.4-175-ga2bb836e55-win64-gpl-4.4/bin/ffmpeg.exe", source="C:/Users/mj_li/Desktop/discord bot/MP3_Files/"+ responses[9]))
+                                        vc.play(discord.FFmpegPCMAudio(executable="MP3_Files/ffmpeg-n4.4-175-ga2bb836e55-win64-gpl-4.4/bin/ffmpeg.exe", source="MP3_Files/"+ responses[9]))
                                         break
                                     elif msg.content.lower().startswith("sigh"):
-                                        vc.play(discord.FFmpegPCMAudio(executable="C:/Users/mj_li/Desktop/discord bot/MP3_Files/ffmpeg-n4.4-175-ga2bb836e55-win64-gpl-4.4/bin/ffmpeg.exe", source="C:/Users/mj_li/Desktop/discord bot/MP3_Files/"+ responses[10]))
+                                        vc.play(discord.FFmpegPCMAudio(executable="MP3_Files/ffmpeg-n4.4-175-ga2bb836e55-win64-gpl-4.4/bin/ffmpeg.exe", source="MP3_Files/"+ responses[10]))
                                         break
                                     else:
                                         choice = random.randint(1,sizeOfDict)
-                                        vc.play(discord.FFmpegPCMAudio(executable="C:/Users/mj_li/Desktop/discord bot/MP3_Files/ffmpeg-n4.4-175-ga2bb836e55-win64-gpl-4.4/bin/ffmpeg.exe", source="C:/Users/mj_li/Desktop/discord bot/MP3_Files/"+ responses[choice]))
+                                        vc.play(discord.FFmpegPCMAudio(executable="MP3_Files/ffmpeg-n4.4-175-ga2bb836e55-win64-gpl-4.4/bin/ffmpeg.exe", source="MP3_Files/"+ responses[choice]))
                                         break
-                                # elif msg.content.lower().startswith("does"):
-                                #     choice = random.randint(1,sizeOfDict)
-                                #     vc.play(discord.FFmpegPCMAudio(executable="C:/Users/mj_li/Desktop/discord bot/ffmpeg-n4.4-175-ga2bb836e55-win64-gpl-4.4/bin/ffmpeg.exe", source="C:/Users/mj_li/Desktop/discord bot/MP3_Files/"+ responses[choice]))
-                                #     break
-                                # elif msg.content.lower().startswith("guh"):
-                                #     vc.play(discord.FFmpegPCMAudio(executable="C:/Users/mj_li/Desktop/discord bot/ffmpeg-n4.4-175-ga2bb836e55-win64-gpl-4.4/bin/ffmpeg.exe", source="C:/Users/mj_li/Desktop/discord bot/MP3_Files/"+ responses[choice]))
-                                #     break
-                                # elif msg.content.lower().startswith("sigh"):
-                                #     vc.play(discord.FFmpegPCMAudio(executable="C:/Users/mj_li/Desktop/discord bot/ffmpeg-n4.4-175-ga2bb836e55-win64-gpl-4.4/bin/ffmpeg.exe", source="C:/Users/mj_li/Desktop/discord bot/MP3_Files/"+ responses[10]))
-                                #     break
-                                # elif msg.content == "is daniel gay":
-                                #     choice = random.randint(1,2)
-                                #     vc.play(discord.FFmpegPCMAudio(executable="C:/Users/mj_li/Desktop/discord bot/ffmpeg-n4.4-175-ga2bb836e55-win64-gpl-4.4/bin/ffmpeg.exe", source="C:/Users/mj_li/Desktop/discord bot/MP3_Files/" + yes[choice]))
-                                #     break
-
+                                
                                 else:
                                     print("Im in")
                                     choice = random.randint(1,sizeOfDict)
-                                    vc.play(discord.FFmpegPCMAudio(executable="C:/Users/mj_li/Desktop/discord bot/ffmpeg-n4.4-175-ga2bb836e55-win64-gpl-4.4/bin/ffmpeg.exe", source=responses[choice]))
+                                    vc.play(discord.FFmpegPCMAudio(executable="MP3_Files/ffmpeg-n4.4-175-ga2bb836e55-win64-gpl-4.4/bin/ffmpeg.exe", source="MP3_Files/"+ responses[choice]))
 
                             elif msg.content == "dc":
                                 await ctx.guild.voice_client.disconnect()
@@ -823,6 +809,7 @@ async def ask(ctx):
                         except:
                             pass
                     else:
+                        print("something went wrong")
                         pass
             except:
                 asyncio.TimeoutError
