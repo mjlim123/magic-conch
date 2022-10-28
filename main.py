f38d172292ac3b5224282b059fb144ff2d2f81b7
Marcus Lim <mj_lim0802@yahoo.com>
2022-10-27 22:46:47 -0500

    added new sound

--- main.py
+++ main.py
@@ -943,7 +943,15 @@ async def snore(ctx):
         await asyncio.sleep(mutagen.mp3.MP3("MP3_Files/snore.mp3").info.length)
         await ctx.guild.voice_client.disconnect()
 
-
+@client.command()
+async def pete(ctx):
+    status = "in channel"
+    if (ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        vc = await channel.connect()
+        vc.play(discord.FFmpegPCMAudio("MP3_Files/pete.mp3"))
+        await asyncio.sleep(mutagen.mp3.MP3("MP3_Files/pete.mp3").info.length)
+        await ctx.guild.voice_client.disconnect()
 
 
 
